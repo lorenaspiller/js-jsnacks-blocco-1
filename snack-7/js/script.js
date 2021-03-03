8529a15ebf1503fa3907f19b1d7ac918133a641b
Lorena Spiller <lorenaspiller@gmail.com>
2021-03-03 13:17:10 +0100

    snack 8

--- snack-7/js/script.js
+++ snack-7/js/script.js
@@ -1,7 +1,14 @@
 /*
-Stampa le potenze di 2 fino a 1000.
+Chiedi un numero di 4 cifre all’utente e calcola la somma
+di tutte le cifre che compongono il numero.
 */
 
-for (var n = 1; n < 1000; n = n * 2) {
-  console.log(n);
+do {
+  var numero = prompt("inserisci il numero");
+} while (!(numero.length >= 1 && numero.length <= 4 ));
+
+var somma = 0;
+for (var i = 0; i < numero.length; i++) {
+  somma += parseInt(numero[i]);
 }
+console.log(somma);
